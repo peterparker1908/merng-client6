3cad9c9c9459b6c99d458fed25343657878931c5
peterparker1908 <phieulang1908@gmail.com>
2021-06-21 11:38:17 +0700

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://shielded-forest-64762.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
